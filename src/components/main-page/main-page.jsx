ab713249e11cf2cdca0d08a02a766fb20605d99d
Nataly <nataly-wien@yandex.ru>
2022-01-12 23:45:43 +0300

    Добавляет в проект графику, шрифты, цвета

---
+++ src/components/main-page/main-page.jsx
@@ -0,0 +1,18 @@
+import './main-page.scss';
+import React from 'react';
+import Header from '../header/header';
+import Footer from '../footer/footer';
+
+const MainPage = () => {
+  return (
+    <div className="main-page page">
+      <Header />
+      <main>
+        <h1 className="visually-hidden">Каталог</h1>
+      </main>
+      <Footer />
+    </div >
+  );
+};
+
+export default MainPage;
